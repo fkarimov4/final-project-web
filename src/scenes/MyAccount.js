11f3ae12fb2ec05191ad9285d066cedb10273637
Farrukh Karimov <fkarimov4@gmail.com>
2022-03-19 09:05:32 -0400

    updated My Account page

--- src/scenes/MyAccount.js
+++ src/scenes/MyAccount.js
@@ -49,9 +49,23 @@ export default function MyAccount() {
                 <p className="text-xl font-bold">{user.displayName}</p>
                 <button
                   onClick={() => logout()}
-                  className="underline hover:opacity-80"
+                  className="flex underline text-slate-600 hover:opacity-80"
                 >
                   Sign Out
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 ml-2 text-slate-600"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+                    />
+                  </svg>
                 </button>
               </div>
             </div>
